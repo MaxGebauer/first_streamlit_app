b560d7ea3816506ef97fe73c282df66fd244f20f
MaxGebauer <110895682+MaxGebauer@users.noreply.github.com>
2022-08-10 15:31:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ streamlit.stop()
 
 #Add Data from user
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
          my_cur.execute("insert into fruit_load_list values ('from streamlit')")
          return "Thanks for adding fruit: " + new_fruit
         
